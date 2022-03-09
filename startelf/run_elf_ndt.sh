b667cc2772d8990a17faffd38925f3f4d5ad2a94
James Cavallo <59240209+jamescavallo@users.noreply.github.com>
2022-03-09 22:24:23 +0000

    Update run_elf_ndt.sh
    Changes

--- startelf/run_elf_ndt.sh
+++ startelf/run_elf_ndt.sh
@@ -1,32 +1,22 @@
 #!/bin/bash
 
 HOST="in"
-INTERFACE="enp5s0f0"
+TARGET="out"
+INTERFACE="enp5s0f0" #this is all setup 
 TSTAMP=`date +%Y%m%d%H%M%S`
 
 euid=`id -u`
 if [[ $euid != "0" ]]; then
-    echo "Gotta be root"
+    echo "Gotta be root" #checks for root access
     exit
 fi
 
-
-rm -f ndthost.sh
-python3 getndt.py -c GB > ndthost.sh
-NDTHOST=`head -1 ndthost.sh | awk '{ print $2 }'`
-if [ -z $NDTHOST ]; then
-	echo "No NDT hosts"
-	exit;
-else
-	echo $NDTHOST
-fi
-
-python3 ~/ELF/elfprobe.py -a ndt7-client -q -l -f ${HOST}_elf_ndt_${TSTAMP} -i ${INTERFACE} -p 10 ${NDTHOST} &
+python3 ~/ELF/elfprobe.py -a iperf -q -l -f ${HOST}_elf_iperf_${TSTAMP} -i ${INTERFACE} -p 10 ${TARGET} &
 ebpfpid=$!
 echo 'sleeping 5 sec; waiting for ELF startup'
 sleep 5
 
-~/go/bin/someta -y someta_ndt.yaml
+~/go/bin/someta -y someta_iperf.yaml
 
 sleep 2
 
